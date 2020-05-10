5034d0cb61293e8205eb5ccb0047b2692c04bb94
Richard Barnes-Webb <richard.barnes-webb@paconsulting.com>
2020-05-10 21:54:41 +0100

    ________________________________________ / master: And a commit that I don't know \ \ the reason of...                       /  ----------------------------------------   \     .    _  .    \    |\_|/__/|        / / \/ \  \       /__|O||O|__ \      |/_ \_/\_/ _\ |      | | (____) | ||      \/\___/\__/  //      (_/         ||       |          ||       |          ||\        \        //_/         \______//        __ || __||       (____(____)

--- app/src/main/java/com/barneswebb/android/fencingcircuit/StopwatchActivity.kt
+++ app/src/main/java/com/barneswebb/android/fencingcircuit/StopwatchActivity.kt
@@ -1,28 +1,29 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    NOTE TO SELF:
+This class is for display only.
+I should have as little logic as possible
+
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 package com.barneswebb.android.fencingcircuit
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.content.*
 import android.graphics.Color
-import android.media.AudioManager
-import android.media.ToneGenerator
-import android.os.*
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Process.THREAD_PRIORITY_BACKGROUND
-import android.util.Log
+import android.os.Bundle
+import android.os.IBinder
 import android.view.View
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.barneswebb.android.fencingcircuit.StopwatchService.StopwatchState.IS_PAUSED
 import com.barneswebb.android.fencingcircuit.StopwatchService.StopwatchState.IS_RUNNING
 import kotlinx.android.synthetic.main.activity_stopwatch.*
-import java.util.stream.Collectors
+
 
 class StopwatchActivity : AppCompatActivity() {
 
     companion object {
         private val TAG: String = StopwatchActivity::class.java.simpleName
-        private val TONE_STREAM = AudioManager.STREAM_ALARM
     }
 
 
@@ -33,12 +34,6 @@ class StopwatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stopwatch)
-
-        HandlerThread("My HandlerThread (y)", THREAD_PRIORITY_BACKGROUND).apply {// from https://developer.android.com/guide/components/services#ExtendingService
-            start()
-            serviceLooper = looper
-            serviceHandler = ServiceHandler(looper)
-        }
     }
 
 
@@ -50,7 +45,6 @@ class StopwatchActivity : AppCompatActivity() {
                 bindService(intent, connection, Context.BIND_AUTO_CREATE)
             }
         }
-        setExText(currentExIdx)//FIRST Ex.
     }
 
     override fun onResume() {
@@ -58,8 +52,15 @@ class StopwatchActivity : AppCompatActivity() {
         if (isBound) {   //TODO This never gets run
             stopwatchStart(exercise_pause)
         }
+        LocalBroadcastManager.getInstance(this).registerReceiver(uiUpdateMessageReceiver,       IntentFilter(StopwatchService.INTENT_NAME_PROC_TICK))
+        LocalBroadcastManager.getInstance(this).registerReceiver(timerDisplayMessageReceiver,   IntentFilter(StopwatchService.INTENT_NAME_UPDATE_TIMESTR));
     }
 
+    override fun onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(uiUpdateMessageReceiver)
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(timerDisplayMessageReceiver)
+        super.onPause()
+    }
 
 //----------------------------------------------------------------------------------//
 //  Button management                                                               //
@@ -83,42 +84,32 @@ class StopwatchActivity : AppCompatActivity() {
     }
 
 
-    var isFirstRun = true
     fun stopwatchStart(playpauseBtn: ImageButton)
     {
         stopwatchService.startStopTimer(IS_RUNNING)
         playpauseBtn.tag = IS_PAUSED.tagText
         playpauseBtn.setImageResource(R.drawable.blueicons_pause)
 
-        serviceHandler?.obtainMessage()?.also { msg ->
-            serviceHandler?.sendMessage(msg)
-        }
-
-        if (isFirstRun) {   //This is pretty ugly TODO refactor
-            repCountList[0] -= 1
-            setExText(currentExIdx)//FIRST Ex.
-            isFirstRun = false
+        if (stopwatchService.isFirstRun) {   //This is pretty ugly TODO refactor
+            stopwatchService.repCountList[0] -= 1
+            setExText(
+                stopwatchService.currentExIdx,
+                "[-]-"
+            )//FIRST Ex.
+            stopwatchService.isFirstRun = false
         }
     }
 
 
-//----------------------------------------------------------------------------------//
-//  Exercise orchestration                                                          //
-//----------------------------------------------------------------------------------//
+    private fun setExText(exIdx: Int, exDisplayString: String)
+    {
+        this_exercise_time.text = exDisplayString
 
-    private var restStarted: Boolean = false
-    private var currentExIdx = 0
-    private var completedExTime: Long = 0
-
-    private var repCountList = DataSource.getDataSet().stream().map { e -> e.noReps }.collect( Collectors.toList())
-
-
-    private fun setExText(exIdx: Int) {
         exercise_name.text = "${exIdx+1}/${DataSource.getDataSet().size}) ${DataSource.getDataSet()[exIdx].exerciseType.title}"
         exercise_desc.text = DataSource.getDataSet()[exIdx].exerciseType.desc
-        remaining_reps.text = "Reps remaining: ${repCountList[exIdx].toString()}"
+        remaining_reps.text = "Reps remaining: ${stopwatchService.repCountList[exIdx].toString()}"
 
-        if (restStarted) {
+        if (stopwatchService.restStarted) {
             exercise_rest.visibility = View.VISIBLE
             next_exercise.visibility = View.VISIBLE
             exercise_rest.setBackgroundColor(Color.YELLOW)
@@ -128,65 +119,6 @@ class StopwatchActivity : AppCompatActivity() {
             exercise_rest.setBackgroundColor(Color.LTGRAY)
             exercise_pause.bringToFront()
         }
-    }
-
-    private fun processTick(elapsedSecs: Long)
-    {
-        Log.d(Companion.TAG, "onChronometerTick> elapsedSecs: $elapsedSecs, restStarted: $restStarted")
-
-        val exTime  = DataSource.getDataSet()[currentExIdx].exTime_s
-        val restT   = DataSource.getDataSet()[currentExIdx].restTime_s
-        val currExT = (elapsedSecs-completedExTime) //ie. total_time - total_time_of_completed_exercises
-
-        this_exercise_time.text = "$currExT/[$exTime+$restT] "
-
-        if ( currExT >= exTime )    //if time elapsed is more than length of current exercise
-        {
-            if ( currExT < (exTime + restT) ) {  //inside rest time
-                if (!restStarted) startRest()
-            } else
-                startNewExercise(elapsedSecs)
-        }
-    }
-
-    private fun getNextExIdx(): Int {
-        if (currentExIdx < (DataSource.list.size-1))
-            return currentExIdx + 1
-        else
-            return 0
-    }
-
-
-    private fun startRest()
-    {
-        Log.d(Companion.TAG, "startRest> ")
-        restStarted = true
-
-        val toneGen1 = ToneGenerator(Companion.TONE_STREAM, 100)
-        toneGen1.startTone(ToneGenerator.TONE_SUP_PIP, 150)
-        Handler().postDelayed({   //2nd beep
-            val toneGen1 = ToneGenerator(Companion.TONE_STREAM, 100)
-            toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 175)
-        }, 150)
-
-        setExText(getNextExIdx())
-    }
-
-
-    private fun startNewExercise(elapsedSecs: Long)
-    {
-        Log.d(Companion.TAG, "startNewExercise> currentExIdx: $currentExIdx")
-        restStarted = false
-
-        val toneGen1 = ToneGenerator(Companion.TONE_STREAM, 100)
-        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
-
-        completedExTime = elapsedSecs  //set completed exercise time
-        currentExIdx = getNextExIdx()
-
-        repCountList[currentExIdx] -=1
-
-        setExText(currentExIdx)
     }
 
 //----------------------------------------------------------------------------------//
@@ -203,41 +135,32 @@ class StopwatchActivity : AppCompatActivity() {
             val binder = service as StopwatchService.LocalBinder
             stopwatchService = binder.getService()
             isBound = true
+
+            setExText(
+                stopwatchService.currentExIdx,
+                "[-]"
+            )//FIRST Ex.
         }
         override fun onServiceDisconnected(arg0: ComponentName) {
             isBound = false
         }
     }
 
-//----------------------------------------------------------------------------------//
-//  Thread to update display                                                        //
-//----------------------------------------------------------------------------------//
-    private var serviceLooper: Looper? = null
-    private var serviceHandler: ServiceHandler? = null
-    private inner class ServiceHandler(looper: Looper) : Handler(looper)//from https://developer.android.com/guide/components/services#ExtendingService
-    {
-        override fun handleMessage(msg: Message)
-        {
-            try
-            {
-                var lastTick = 0
-                while (stopwatchService.stopwatchState == IS_RUNNING)
-                {
-                    this@StopwatchActivity.runOnUiThread( Runnable {
-                        var elapsedSecs = stopwatchService.elapsedTime()/1_000
-                        if (elapsedSecs >= lastTick) {
-                            processTick(elapsedSecs)
-                            lastTick = elapsedSecs.toInt()
-                        }
-                        stopwatch.text = stopwatchService.elapsedTimeStr()
-                    })
 
-                    Thread.sleep(50)
-                }
-            } catch (e: InterruptedException) {
-                Log.e(TAG, "Error during time_display update: ", e)
-                Thread.currentThread().interrupt()                // Restore interrupt status.
-            }
+    // https://stackoverflow.com/questions/30629071/sending-a-simple-message-from-service-to-activity
+    private val uiUpdateMessageReceiver: BroadcastReceiver = object : BroadcastReceiver()
+    {
+        override fun onReceive(context: Context, intent: Intent) {
+            setExText(
+                intent.getIntExtra(StopwatchService.INTENTXTR_EXIDX_TO_DISPL, -1),
+                intent.getStringExtra(StopwatchService.INTENTXTR_CUREX_T_DISPL)
+            )
+        }
+    }
+    private val timerDisplayMessageReceiver: BroadcastReceiver = object : BroadcastReceiver()
+    {
+        override fun onReceive(context: Context, intent: Intent) {
+            stopwatch.text = intent.getStringExtra(StopwatchService.INTENT_NAME_UPDATE_TIMESTR)
         }
     }
 
--- app/src/main/java/com/barneswebb/android/fencingcircuit/StopwatchService.kt
+++ app/src/main/java/com/barneswebb/android/fencingcircuit/StopwatchService.kt
@@ -1,41 +1,48 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    NOTE TO SELF:
+This class should contain all the timing & exercise logic
+The attached clients/activities should be 'dumb Views'
+
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 package com.barneswebb.android.fencingcircuit
 
 import android.app.*
 import android.content.Intent
-import android.os.Binder
-import android.os.IBinder
+import android.media.AudioManager
+import android.media.ToneGenerator
+import android.os.*
 import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import java.util.stream.Collectors
 
 
 class StopwatchService : Service() {
 
     companion object {
-        val TAG: String = StopwatchService::class.java.simpleName
-        const val ONGOING_NOTIFICATION_ID: Int = 1234
-        const val CHANNEL_ID: String = "testing testing"
-    }
+        private val TAG: String = StopwatchService::class.java.simpleName
+        private val CHANNEL_ID: String = "$TAG:NotifChanl"
+        private const val ONGOING_NOTIFICATION_ID: Int = 1234
+        private const val TONE_STREAM = AudioManager.STREAM_ALARM
 
-    private val binder = LocalBinder()// Binder given to clients
-    inner class LocalBinder : Binder() {
-        fun getService(): StopwatchService = this@StopwatchService// Return this instance of LocalService so clients can call public methods
-    }
-
-    override fun onBind(intent: Intent?): IBinder? {
-        Log.d(TAG, "onBind(${intent}) & startForeground = = = = = = = = = = =")
-        startForegroundService(intent) //trigger onStartCommand()
-        return binder
-    }
-
-    override fun onUnbind(intent: Intent): Boolean {
-        stopSelf()
-        return false
+        const val INTENT_NAME_UPDATE_TIMESTR= "stopwatchTimeStr"
+        const val INTENT_NAME_PROC_TICK     = "exerciseIdxToDisplay"
+        const val INTENTXTR_EXIDX_TO_DISPL  = "exIdx"
+        const val INTENTXTR_CUREX_T_DISPL   = "thisExProgressStr"
     }
 
 
 
-
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int
+    {
         Log.v(TAG, "Starting service ========================================")
+
+
+        HandlerThread("ClockTickHandlerThread", Process.THREAD_PRIORITY_BACKGROUND).apply {// from https://developer.android.com/guide/components/services#ExtendingService
+            start()
+            tickLooper = looper
+            tickHandler = ClockTickHandler(looper)
+        }
 
         val pendingIntent: PendingIntent =
             Intent(this, StopwatchActivity::class.java).let { notificationIntent ->
@@ -62,11 +69,62 @@ class StopwatchService : Service() {
         getSystemService(NotificationManager::class.java)!!
             .createNotificationChannel(
                 NotificationChannel(
-                CHANNEL_ID,
-                "Foreground Service Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            ) )
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                ) )
     }
+
+
+//----------------------------------------------------------------------------------//
+//  Thread to tell clients to update display                                        //
+//----------------------------------------------------------------------------------//
+    private var tickLooper: Looper? = null
+    private var tickHandler: ClockTickHandler? = null
+    private inner class ClockTickHandler(looper: Looper) : Handler(looper)//from https://developer.android.com/guide/components/services#ExtendingService
+    {
+        override fun handleMessage(msg: Message)
+        {
+            try
+            {
+                var lastTick = 0
+                while (stopwatchState == StopwatchState.IS_RUNNING)
+                {
+                    val elapsedSecs = elapsedTime()/1_000
+                    if (elapsedSecs >= lastTick) {
+                        processTick(elapsedSecs)
+                        lastTick = elapsedSecs.toInt()
+                    }
+                    broadcastElapsedTimeStr()
+                    Thread.sleep(50)
+                }
+            } catch (e: InterruptedException) {
+                Log.e(TAG, "Error during time_display update: ", e)
+                Thread.currentThread().interrupt()                // Restore interrupt status.
+            }
+        }
+    }
+
+//----------------------------------------------------------------------------------//
+//  Connection/binding stuff                                                                 //
+//----------------------------------------------------------------------------------//
+
+    private val binder = LocalBinder()// Binder given to clients
+    inner class LocalBinder : Binder() {
+        fun getService(): StopwatchService = this@StopwatchService// Return this instance of LocalService so clients can call public methods
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        Log.d(TAG, "onBind(${intent}) & startForeground = = = = = = = = = = =")
+        startForegroundService(intent) //trigger onStartCommand()
+        return binder
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        stopSelf()
+        return false
+    }
+
 
 
 //----------------------------------------------------------------------------------//
@@ -78,6 +136,8 @@ class StopwatchService : Service() {
         IS_RUNNING("Play")
     }
 
+    var isFirstRun = true
+
     // Start and end times in milliseconds
     private var startTime: Long = 0  // Start and end times in milliseconds
     private var pauseTime: Long = 0
@@ -88,10 +148,14 @@ class StopwatchService : Service() {
 
     fun startStopTimer(targetState: StopwatchState) {
         when (targetState) {
-            StopwatchState.IS_RUNNING ->  startTime = System.currentTimeMillis() - pauseTime
-            StopwatchState.IS_PAUSED ->  pauseTime = elapsedTime()
+            StopwatchState.IS_RUNNING ->    startTime = System.currentTimeMillis() - pauseTime
+            StopwatchState.IS_PAUSED ->     pauseTime = elapsedTime()
         }
         stopwatchState = targetState
+
+        tickHandler?.obtainMessage()?.also { msg ->
+            tickHandler?.sendMessage(msg)
+        }
     }
 
     fun resetTimer() {
@@ -105,15 +169,97 @@ class StopwatchService : Service() {
     }
 
 
-    fun elapsedTimeStr(): String   // idea stolen from https://coderwall.com/p/wkdefg/converting-milliseconds-to-hh-mm-ss-mmm
-    {
-        var elapsedTime =   elapsedTime()
-        var milliseconds =  (elapsedTime%1000)/10
-        var seconds =       (elapsedTime/1000)%60
-        var minutes =       (elapsedTime/(1000*60))%60
-        var hours =         (elapsedTime/(1000*60*60))%24
 
-        return "%02d:%02d:%02d.%02d".format(hours, minutes, seconds, milliseconds)//
+    fun broadcastElapsedTimeStr()
+    {
+        val intent = Intent(INTENT_NAME_UPDATE_TIMESTR)
+
+        val elapsedTime =   elapsedTime()
+        val milliseconds =  (elapsedTime%1000)/10
+        val seconds =       (elapsedTime/1000)%60
+        val minutes =       (elapsedTime/(1000*60))%60
+        val hours =         (elapsedTime/(1000*60*60))%24
+
+        intent.putExtra(INTENT_NAME_UPDATE_TIMESTR, "%02d:%02d:%02d.%02d".format(hours, minutes, seconds, milliseconds))
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
+
+
+//----------------------------------------------------------------------------------//
+//  Exercise orchestration                                                          //
+//----------------------------------------------------------------------------------//
+
+    var restStarted: Boolean = false
+    var currentExIdx = 0
+    private var completedExTime: Long = 0
+
+    var repCountList: MutableList<Int> = DataSource.getDataSet().stream().map { e -> e.noReps }.collect( Collectors.toList())
+
+
+    private fun processTick(elapsedSecs: Long)
+    {
+        Log.d(TAG, "onChronometerTick> elapsedSecs: $elapsedSecs, restStarted: $restStarted")
+
+        val exTime  = DataSource.getDataSet()[currentExIdx].exTime_s
+        val restT   = DataSource.getDataSet()[currentExIdx].restTime_s
+        val currExT = (elapsedSecs-completedExTime) //ie. total_time - total_time_of_completed_exercises
+
+        if ( currExT >= exTime )    //if time elapsed is more than length of current exercise
+        {
+            if ( currExT < (exTime + restT) ) {  //inside rest time
+                if (!restStarted) startRest()
+            } else {
+                startNewExercise(elapsedSecs)
+            }
+        }
+
+        val intent = Intent(INTENT_NAME_PROC_TICK)
+        if (restStarted) {
+            intent.putExtra(INTENTXTR_EXIDX_TO_DISPL, getNextExIdx()) //if resting, override current & display next ex.
+        } else {
+            intent.putExtra(INTENTXTR_EXIDX_TO_DISPL, currentExIdx)
+        }
+        intent.putExtra(INTENTXTR_CUREX_T_DISPL, "$currExT/[$exTime+$restT]")
+
+        // https://stackoverflow.com/questions/30629071/sending-a-simple-message-from-service-to-activity
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+    }
+
+    private fun getNextExIdx(): Int {
+        if (currentExIdx < (DataSource.list.size-1))
+            return currentExIdx + 1
+        else
+            return 0
+    }
+
+
+    private fun startRest()
+    {
+        Log.d(TAG, "startRest> ")
+        restStarted = true
+
+        val toneGen1 = ToneGenerator(TONE_STREAM, 100)
+        toneGen1.startTone(ToneGenerator.TONE_SUP_PIP, 150)
+        Handler().postDelayed({   //2nd beep
+            val toneGen2 = ToneGenerator(TONE_STREAM, 100)
+            toneGen2.startTone(ToneGenerator.TONE_CDMA_PIP, 175)
+        }, 150)
+
+    }
+
+
+    private fun startNewExercise(elapsedSecs: Long)
+    {
+        Log.d(TAG, "startNewExercise> currentExIdx: $currentExIdx")
+        restStarted = false
+
+        val toneGen1 = ToneGenerator(TONE_STREAM, 100)
+        toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP, 150)
+
+        completedExTime = elapsedSecs  //set completed exercise time
+        currentExIdx = getNextExIdx()
+
+        repCountList[currentExIdx] -=1
     }
 
 
